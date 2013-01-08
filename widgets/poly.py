22782899408ab91dd9e499422414f815dc0ff1a9
Nathaniel Case <Qalthos@gmail.com>
2013-01-08 18:26:38 -0500

    Initial pull of polygrid bits from CIVX

---
+++ widgets/poly.py
@@ -0,0 +1,136 @@
+# This file is part of CIVX.
+#
+# CIVX is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# CIVX is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with CIVX.  If not, see <http://www.gnu.org/licenses/>.
+#
+# Copyright 2009-2010, CIVX, Inc.
+"""
+The CIVX Poly Grid
+==================
+"""
+
+import logging
+
+from tw.api import js_callback
+from tw.jquery import jQuery
+
+from civx.utils import get_knowledge_session, get_colmodel_from_entity, get_colnames_from_entity
+from civx.widgets.jqgrid import JQueryGrid
+
+from civx.knowledge.model import Entity
+
+log = logging.getLogger(__name__)
+
+class PolyGrid(JQueryGrid):
+    params = ['entity']
+
+    template = """
+<div>
+  <div class="gridfooter" align="right">
+    ## TODO:
+    ## - render custom links from facts about the entity
+
+    <ul>
+        % if entity:
+            <h1>${entity.title}</h1>
+            <h2>Facts</h2>
+            % for fact in entity.facts:
+                % if entity[fact].startswith('http'):
+                    <li><b>${fact}:</b> <a href="${entity[fact]}">${entity[fact]}</a></li>
+                % else:
+                    <li><b>${fact}:</b> ${entity[fact]}</li>
+                % endif
+            % endfor
+         % endif
+    </ul>
+
+    ##% if model:
+    ##    % if model.__name__ in graphs:
+    ##      <a href="#" onclick="civx_new_tab_iframe('#${model.__name__}_metrics', '${model.__name__} Metrics', '/topN/${model.__name__}'); return false;"><span class="ss_sprite ss_chart_bar">Metrics</span></a>
+    ##      <a href="/feeds/${model.__name__}"><span class="ss_sprite ss_feed">Feed</span></a>
+    ##      <a href="/documentation/models/${model.__name__}"><span class="ss_sprite ss_report">Docs</span></a>
+    ##    % endif
+    ##    % if git_repo:
+    ##      <a href="#" onclick="civx_new_tab_iframe('#${model.__name__}_raw', '${model.__name__} Raw Data', '${gitweb}/?p=civx-csv&a=tree&f=${git_repo}'); return false;"><span class="ss_sprite ss_page_white_text">Raw</span></a>
+    ##    % endif
+    ##    <a href="#" onclick="moksha.view_module_source('${model_module}'); return false;"><span class="ss_sprite ss_database_gear">Model Source</span></a>
+    ##    <a href="#" onclick="moksha.view_module_source('${scraper_module}'); return false;"><span class="ss_sprite ss_script_gear">Scraper Source</span></a>
+    ##    <a href="#" onclick="moksha.view_module_source('${gridname}'); return false;"><span class="ss_sprite ss_brick">Widget Source</span></a>
+    ##    <a href="#" onclick="civx_new_tab('#Attribution', 'Attribution', '/widgets/entourage'); return false;"><span class="ss_sprite ss_information">Attribution</span></a>
+    ##    <a href="#" onclick="civx_embed_widget_dialog('${gridname}'); return false;"><span class="ss_sprite ss_html_add">Embed</span></a>
+    ##% endif
+  </div>
+  <br/>
+  <table id="${id}" class="scroll" cellpadding="0" cellspacing="0"></table>
+  <div id="${id}_pager" class="scroll" style="text-align:center;"></div>
+</div>
+    """
+    engine_name = 'mako'
+
+    def update_params(self, d):
+        super(PolyGrid, self).update_params(d)
+
+        Knowledge = get_knowledge_session()
+        try:
+            entity = Knowledge.query(Entity).filter_by(name=d.model).one()
+        except Exception, e:
+            log.warning(e)
+            entity = Knowledge.query(Entity).filter_by(name=d.model).first()
+
+        colNames = get_colnames_from_entity(entity)
+        colModel = get_colmodel_from_entity(entity)
+
+        params = {
+            'url': '/grids/query?widget=' + entity.name,
+            'datatype': 'json',
+            'colNames': colNames,
+            'colModel': colModel,
+            'height': 550,
+            'rowNum': 25,
+            #'rowList': [10,20,30],
+            'pager': '#%s_pager' % d.id,
+            'sortname': 'id',
+            'viewrecords': True,
+            'sortorder': "desc",
+            'caption': entity.name,
+            'autowidth': True,
+            'loadui': 'block',
+            #'shrinkToFit': True,
+            'forcefit': True,
+            }
+
+        self.add_call(jQuery('#' + d.id)
+                        .jqGrid(params)
+                        .navGrid('#%s_pager' % d.id, {
+                            'add': False,
+                            'del': False,
+                            'edit': False,
+                            'search': False,
+                            }, {}, {}, {}, {'multipleSearch': True}))
+
+        self.add_call(jQuery('#' + d.id)
+                        .jqGrid('filterToolbar',{
+                            'stringResult': True,
+                            'searchOnEnter': False
+                            }));
+
+        self.add_call(jQuery('#' + d.id)
+                        .jqGrid('navButtonAdd', '#%s_pager' % d.id, {
+                            'caption': 'Columns',
+                            'title': 'Reorder Columns',
+                            'onClickButton': js_callback("""
+                                function() {
+                                    jQuery('#%s').jqGrid('columnChooser');
+                                }
+                            """ %  d.id),
+                            }))
---
+++ widgets/widget.py
@@ -0,0 +1,109 @@
+# This file is part of CIVX.
+#
+# CIVX is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# CIVX is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with CIVX.  If not, see <http://www.gnu.org/licenses/>.
+#
+# Copyright 2009-2010, CIVX, Inc.
+"""
+The CIVX Grid Widget
+====================
+"""
+
+from uuid import uuid4
+from inspect import isclass
+from tg import config
+from tw.api import Widget, JSLink, CSSLink, js_callback
+from tw.jquery import jquery_js, jQuery
+
+from civx.widgets.blueprint import blueprint_sprites_css
+from civx.widgets.blueprint import blueprint_screen_css, blueprint_print_css
+from moksha.lib.helpers import when_ready
+
+import civx
+from civx.widgets.civx_js import civx_js
+from civx.widgets.style import civx_css
+from civx.widgets.civx_menu import CIVXMenu
+from civx.controllers.topN import graphs
+
+jquery_ui_all_js = JSLink(filename='static/js/jquery-ui-1.8.2.custom.min.js',
+                          modname=__name__)
+#jquery_ui_all_js = JSLink(filename='static/js/jquery-ui-1.7.1.custom.min.js',
+#                          modname=__name__)
+
+jquery_layout_js = JSLink(filename='static/js/jquery.layout.js',
+                          modname=__name__)
+jqgrid_locale_en = JSLink(filename='static/js/i18n/grid.locale-en.js',
+                          modname=__name__)
+jquery_tablednd_js = JSLink(filename='static/js/jquery.tablednd.js',
+                            modname=__name__)
+jquery_modal_js = JSLink(filename='static/js/jqModal.js',
+                         modname=__name__)
+jquery_dnr_js = JSLink(filename='static/js/jqDnR.js',
+                       modname=__name__)
+jqgrid_contextmenu_js = JSLink(filename='static/js/jquery.contextmenu.js',
+                               modname=__name__)
+jqgrid_multiselect_js = JSLink(filename='static/js/ui.multiselect.js',
+                               modname=__name__)
+jquery_jqgrid_js = JSLink(filename='static/js/jquery.jqGrid.js',
+              javascript=[jqgrid_multiselect_js],
+                          modname=__name__)
+
+lightness_ui_css = CSSLink(filename='static/themes/start/jquery-ui.custom.css',
+                           modname=__name__, media='screen')
+ui_jqgrid_css = CSSLink(filename='static/themes/ui.jqgrid.css',
+                        modname=__name__, media='screen')
+
+ui_multiselect_css = CSSLink(filename='static/themes/ui.multiselect.css',
+                        modname=__name__, media='screen')
+jqgrid_fluid_js = JSLink(filename='static/js/jquery.jqGrid.fluid.js', modname=__name__)
+
+gitweb_url = config.get('civx.gitweb.url',
+                        'http://civx-git1.csh.rit.edu/repoweb')
+
+class JQueryGrid(Widget):
+    params = ['id', 'model', 'gridname', 'graphs', 'git_repo', 'gitweb',
+              'model_module', 'scraper_module', 'civx_menu', 'menu']
+    template = 'mako:civx.widgets.jqgrid.templates.jqgrid'
+    javascript = [jquery_js, jquery_ui_all_js, jquery_layout_js,
+                  jqgrid_multiselect_js,
+                  jqgrid_locale_en, jquery_jqgrid_js,
+                  #jquery_tablednd_js,#jqgrid_contextmenu_js,
+                  civx_js]
+    css = [lightness_ui_css, ui_jqgrid_css,
+           blueprint_screen_css, blueprint_print_css, blueprint_sprites_css,
+           civx_css, ui_multiselect_css]
+
+    civx_menu = False # Whether or not to display the menu
+    menu = CIVXMenu('civx_menu')
+
+    def update_params(self, d):
+        """ Assign a unique ID to this widget when it is rendered """
+        super(JQueryGrid, self).update_params(d)
+        if not d.id:
+            d.id = str(uuid4())
+        d.gridname = self.__class__.__name__
+        d.graphs = graphs
+        if d.model:
+            if isinstance(d.model, basestring):
+                civx_model = civx.utils.get_model(d.model)
+                if civx_model:
+                    d.model = civx_model
+            if isclass(d.model):
+                d.git_repo = civx.utils.get_git_repo_from_model(d.model)
+                d.gitweb = gitweb_url
+                d.model_module = d.model.__module__ + '.' + d.model.__name__
+                scraper = civx.utils.get_scraper_from_model(d.model)
+                if scraper:
+                    d.scraper_module = scraper.__module__ + '.' + scraper.__name__
+
+# vim: expandtab sw=4 ts=4 ai

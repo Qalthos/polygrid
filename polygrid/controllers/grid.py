0821bfe2e05a1155cac1a14730ec6068808050b2
Nathaniel Case <Qalthos@gmail.com>
2013-01-14 16:26:36 -0500

    Hook up searching.
    Note: searching works on the actual columns of the data, not the displayed columns which are different for some reason.

--- polygrid/controllers/grid.py
+++ polygrid/controllers/grid.py
@@ -1,3 +1,6 @@
+import json
+
+from sqlalchemy import and_, or_, func
 from tg import expose
 
 from knowledge.model import Entity
@@ -11,14 +14,33 @@ class GridController(object):
     def query(self, like, sidx, rows=25, sord='desc', page=1, _search=False, **kw):
         Knowledge = get_knowledge_session("sqlite:///knowledge.db")
         query = Knowledge.query(Entity).filter(Entity.name.like(like))
+
+        model, columns = get_colmodel_and_colnames_from_entity(Knowledge, like)
+
+        if _search and _search.lower() == 'true':
+
+            if 'filters' in kw:
+                filters = json.loads(kw['filters'])
+                if filters['groupOp'] == 'AND':
+                    group_func = and_
+                elif filters['groupOp'] == 'OR':
+                    group_func = or_
+
+                multi_query = []
+                for rule in filters['rules']:
+                    col = getattr(Entity, rule['field'])
+                    multi_query.append(self.query_filter(rule['op'],
+                                                         col,
+                                                         rule['data']))
+                query = query.filter(group_func(*multi_query))
+
+
         count = query.count()
         page = int(page)
         rows = int(rows)
         total_pages = count / rows
         if page > total_pages:
             page = total_pages
-
-        model, columns = get_colmodel_and_colnames_from_entity(Knowledge, like)
 
         # This goes before offset, if it gets fixed
         #~ .order_by(getattr(getattr(model, sidx), sord)()) \
@@ -38,3 +60,23 @@ class GridController(object):
             entries.append({'id': row.id, 'cell': cell})
 
         return dict(page=page, total=total_pages, records=count, rows=entries)
+
+    def query_filter(self, oper, col, string):
+        if oper == 'eq':
+            return col == string
+        elif oper == 'ne':
+            return col != string
+        elif  oper == 'lt':
+            return col < string
+        elif oper == 'le':
+            return col <= string
+        elif oper == 'gt':
+            return col > string
+        elif oper == 'ge':
+            return col >= string
+        elif oper == 'bw':
+            return func.lower(col).like(string.lower() + '%')
+        elif oper == 'ew':
+            return func.lower(col).like('%' + string.lower())
+        elif oper == 'cn':
+            return func.lower(col).like('%' + string.lower() + '%')
